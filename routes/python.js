64748f8cb0006fde5dd4eb712c08015cf6450960
hwangtamu <whxjtu@gmail.com>
2018-04-03 00:07:43 -0500

    python

--- routes/python.js
+++ routes/python.js
@@ -4,12 +4,11 @@ var fs = require('fs');
 var router = express.Router();
 var obj = fs.readFileSync('data/python.json');
 var pythonData = JSON.parse(obj);
-console.log(pythonData.pythonData);
 
 router.get('/', function(req, res, next) {
 
     res.render('python',
-    { title : 'Python', pythonData: pythonData.pythonData})
+    { title : 'Python', pythonData: pythonData})
 });
 
 module.exports = router;
