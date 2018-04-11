4803bcc0f0d04871a23f816f15987c80d850e5c4
Han <whxjtu@gmail.com>
2018-04-11 15:22:34 -0500

    callback fixed

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,30 @@ var gists = new Gists({
     username: 'hwangtamu',
     password: 'wh07092053'
 });
-var url = "http://api.github.com/gists/public";
 var gistData;
-var get_gists = function (name, query, cb) {
+var url = "http://api.github.com/gists/public";
+
+var get_gists = function (cb) {
     gists.allPublic({}, function(err, res){
         gistData = res.slice(0, 5).sort(function(a,b){
-            console.log(gistData);
             return parseInt(Date.parse(a.modified_at)) - parseInt(Date.parse(b.modified_at));
         });
+        cb(gistData);
     });
 };
 
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    get_gists();
-    res.render('index',
-        { title : 'Home', gistData: gistData }
-    )
+    // get_gists();
+    // res.render('index',
+    //     { title : 'Home', gistData: gistData }
+    // )
+    get_gists(function(gistData){
+        res.render('index',
+            { title : 'Home', gistData: gistData})
+    });
+
 })
 
 module.exports = router;
