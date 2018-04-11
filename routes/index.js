ffce08acc1010360e41bf1c7bbdd24a9a33bf494
Han <whxjtu@gmail.com>
2018-04-10 19:19:56 -0500

    init

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var Gists = require('gists');
+var gists = new Gists({
+    username: 'hwangtamu',
+    password: 'wh07092053'
+});
+var url = "http://api.github.com/gists/public";
+var gistData;
+var get_gists = function (name, query, cb) {
+    gists.allPublic({}, function(err, res){
+        gistData = res.slice(0, 5).sort(function(a,b){
+            return parseInt(Date.parse(a.modified_at)) - parseInt(Date.parse(b.modified_at));
+        });
+    });
+};
+
 
 /* GET home page. */
 router.get('/', function (req, res) {
+    get_gists();
     res.render('index',
-        { title : 'Home' }
+        { title : 'Home', gistData: gistData }
     )
 })
 
