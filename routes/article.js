d6035675aedb4bcf9ad962c7a8e1a7b2ef1a1af0
Han <whxjtu@gmail.com>
2018-10-17 18:55:56 -0500

    add tutors

--- routes/article.js
+++ routes/article.js
@@ -14,16 +14,22 @@ function get_file(n){
     return pythonData[n];
 }
 
+var reg = /tutorial.+/;
+
 router.get('/:name', function(req, res, next) {
     var n = req.params.name;
-    if(n.charCodeAt(0)<58){
+    if(n.match(reg)){
+        res.render('tutorial', {
+            articleData: 'data/static/'+n+'.html'
+        })
+    }else if(n.charCodeAt(0)<58){
         find('python', {}, function(err, docs){
             pythonData = docs[0];
             res.render('article',
                 { title : get_file(n).title, articleData: get_file(n)})
         });
     }else{
-        console.log(n);
+        //console.log(n);
         var data = fs.readFileSync('data/article/'+n);
         res.contentType("application/pdf");
         res.send(data);
