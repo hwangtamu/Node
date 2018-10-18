0d27dfbed74d06c39635cb148d4dffd85eecb167
Han <whxjtu@gmail.com>
2018-10-18 09:51:20 -0500

    add tutors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.favicon("public/images/favicon.ico"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
